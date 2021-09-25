96d08344f6d7acb48b45d10d0cdea6c6153518c5
florencetang <80378575+florencetang@users.noreply.github.com>
2021-09-26 01:22:48 +1000

    fixed dots

--- map.js
+++ map.js
@@ -99,14 +99,12 @@ infoWindow.setContent(
 infoWindow.open(map);
 }
 
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
 
+// SEARCH
 
 function initAutocomplete() {
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -33.8688, lng: 151.2195},
+    center: {lat: -37.8180, lng: 144.9691},
     zoom: 13,
     mapTypeId: 'roadmap'
   });
@@ -169,4 +167,78 @@ function initAutocomplete() {
     });
     map.fitBounds(bounds);
   });
+
+  
+for (const city in citydmap) {
+  // Add the circle for this city to the map.
+  const cityCircle = new google.maps.Circle({
+    strokeColor: "#74099e",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#af48c9",
+    fillOpacity: 0.35,
+    map,
+    center: citydmap[city].center,
+    radius: Math.sqrt(citydmap[city].darklevel) * 100,
+  });
+}
+
+  infoWindow = new google.maps.InfoWindow();
+
+  const locationButton = document.createElement("button");
+
+locationButton.textContent = "Pan to Current Location";
+locationButton.classList.add("custom-map-control-button");
+map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+locationButton.addEventListener("click", () => {
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent("Location found.");
+        infoWindow.open(map);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+});
+
+  const walkPlanCoordinates = [
+  { lat: -37.83, lng: 144.9691 },
+  { lat: -37.824, lng: 145.002 },
+  { lat: -37.81, lng: 145 },
+];
+const walkPath = new google.maps.Polyline({
+  path: walkPlanCoordinates,
+  geodesic: true,
+  strokeColor: "lightgreen",
+  strokeOpacity: 1.0,
+  strokeWeight: 4,
+});
+
+walkPath.setMap(map);
+
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+infoWindow.setPosition(pos);
+infoWindow.setContent(
+  browserHasGeolocation
+    ? "Error: The Geolocation service failed."
+    : "Error: Your browser doesn't support geolocation."
+);
+infoWindow.open(map);
 }
